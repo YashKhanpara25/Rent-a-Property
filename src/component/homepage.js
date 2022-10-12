a3bd35a6b33d9aacb823b8fc6f2a6af168817dbd
YashKhanpara <yashkhanpara25@gmail.com>
2022-10-12 18:22:42 +0530

    implemented a navbar and footer component

---
+++ src/component/homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from './layout';
+
+
+const Homepage = () => {
+  return (
+    <div>
+        <Layout>
+            <HouseList  />
+        </Layout>
+    </div>
+  )
+}
+
+export default Homepage;
